702a867584f6bcb143a407177508afbe5429bad5
Tipton <george.tipton@ecolab.com>
2021-09-17 11:35:08 -0500

    38. Using the Node Modules System

---
+++ udemy-node-1/routes.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    //get request data
+    //on allows us to listen to specific events
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    }); //fired whenever a new chunk is ready to be read
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      //will wait for file write before proceeding (bad)
+      //fs.writeFileSync("udemy-node-1/message.txt", message);
+      fs.writeFile("udemy-node-1/message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Welcome</title></head>");
+  res.write("<body><h1>Welcome to Node.JS</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+//make the function accessible outside file
+module.exports = requestHandler;
