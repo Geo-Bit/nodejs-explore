f65bf6cb92d8a04048e7c56fcf5e281422887688
Tipton <george.tipton@ecolab.com>
2021-09-20 16:43:21 -0500

    excluding modules

--- udemy-node-1/app.js
+++ udemy-node-1/app.js
@@ -1,5 +1,27 @@
 const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action = "/product" method = "POST"><input type="text" name = "title"><button type = "submit">Add Product</button></input></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Home</h1>");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
