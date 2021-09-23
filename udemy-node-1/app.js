bd57dfbd94a3907c8bf7781c60835a54fd1b07b7
Tipton <george.tipton@ecolab.com>
2021-09-23 17:03:35 -0500

    87. Avoiding an Error (Pug)

--- udemy-node-1/app.js
+++ udemy-node-1/app.js
@@ -3,17 +3,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const adminRoutes = require("./routes/admin");
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 //404 code response
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
