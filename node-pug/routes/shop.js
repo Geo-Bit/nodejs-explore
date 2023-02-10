c87ea7cb30a4a594fbbaf864de200a2c450d2499
George Tipton <gtip22@gmail.com>
2023-02-09 18:00:00 -0700

    renamed project, updated file names, etc.

---
+++ node-pug/routes/shop.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path");
+const router = express.Router();
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "My Shop", path: "/" });
+});
+
+module.exports = router;
